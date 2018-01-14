21884836125c503269adaebe976cd56498795926
Rafał Wawrowicz <rwawrowicz@gmail.com>
2018-01-14 18:47:53 +0100

    TextView's added

--- app/src/main/java/com/example/wawr1/hny/MainActivity.java
+++ app/src/main/java/com/example/wawr1/hny/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.wawr1.hny;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void getCurrentYear(android.view.View view) {
+        TextView current_year_view = (TextView)findViewById(R.id.current_year);
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        current_year_view.setText(String.valueOf(year));
+    }
+
+
+
+
 }
